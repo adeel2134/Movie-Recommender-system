bb6b8bd01c800fc87c23a4658364bf47ebdc9879
adeel2134 <madeel0t1@gmail.com>
2026-04-22 08:50:51 +0500

    app.py

--- app.py
+++ app.py
@@ -104,16 +104,16 @@ def load_model():
 
     # Load from Google Drive
     import gdown
-import os
+    import os
 
-if not os.path.exists('tmdb_5000_movies.csv'):
+    if not os.path.exists('tmdb_5000_movies.csv'):
     gdown.download('https://drive.google.com/uc?id=14moVMaytcrxJdyWmsCAHW0f0Je8NU3F0', 'tmdb_5000_movies.csv', quiet=False)
 
-if not os.path.exists('tmdb_5000_credits.csv'):
+    if not os.path.exists('tmdb_5000_credits.csv'):
     gdown.download('https://drive.google.com/uc?id=1FUzB655ZdePzgJzhnFsat6wd0XFBKVq-', 'tmdb_5000_credits.csv', quiet=False)
 
-movies = pd.read_csv('tmdb_5000_movies.csv')
-credits = pd.read_csv('tmdb_5000_credits.csv')
+    movies = pd.read_csv('tmdb_5000_movies.csv')
+    credits = pd.read_csv('tmdb_5000_credits.csv')
 
     movies = movies.merge(credits, on='title')
     movies = movies[['movie_id', 'title', 'genres', 'keywords', 'overview', 'cast', 'crew']]
